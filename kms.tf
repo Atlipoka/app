5a6d4f6ad9d549c0b17999dbadf0390d989000d8
vagrant <vagrant@vagrant.vm>
2023-12-11 11:08:56 +0000

    Create file kms.tf and edit vars.tf

---
+++ kms.tf
@@ -0,0 +1,5 @@
+resource "yandex_kms_symmetric_key" "kms" {
+  name              = "kms"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h"
+}
--- vars.tf
+++ vars.tf
@@ -1,9 +1,3 @@
-resource "yandex_kms_symmetric_key" "kms" {
-  name              = "kms"
-  default_algorithm = "AES_128"
-  rotation_period   = "80h"
-}
-
 data "yandex_iam_service_account" "sa" {
   name     = "terraform"
 }
