edb79209053af64cf3f539ffec31122ed03c578a
vagrant <vagrant@vagrant.vm>
2023-12-13 11:37:19 +0000

    Create outputs.tf

--- bucket.tf
+++ bucket.tf
@@ -18,13 +18,3 @@ resource "yandex_storage_bucket" "kabaev-bucket" {
     }
   }
 }
-
-output "access_key" {
-  value     = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  sensitive = true
-}
-
-output "secret_key" {
-  value     = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  sensitive = true
-}
